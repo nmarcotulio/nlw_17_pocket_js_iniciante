b5f9101a1b7aa38ac9842ff5fb512c4694d4b508
“nmarcotulio” <marcotulionogueira@gmail.com>
2024-09-09 21:16:54 -0300

    aula 01

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
-// hello world
-const mensagem = "olá mundo!"
+// olá, mundo!
+console.log("olá, mundo!")
 
+
+// variáveis - let
+let mensagens = "olá, mundo!"
+
+
+// variáveis - const
+const mensagem = "olá, mundo"
 {
-    const mensagem = "hello world"
+    const mensagem = "olá, Tulio!"
     console.log(mensagem)
 }
-
 console.log(mensagem);
+
+
+// arrays
+let metas = ['tulio', 'alo']
+let metas01 = [2, 'tulio']
+// concatenando valores
+console.log(metas[1] + ", " + metas[0]) 
+
+let metas02 = [
+    metas,
+    {
+        value: 'caminhar 20 minutos todos os dias',
+        checked: false
+    }
+]
+console.log(metas02[1].value);
+
+// objetos
+let meta = {
+    value: 'ler um livro por mês',
+    address: 2,
+    checked: true,
+    isChecked: () => {
+        console.log(info)
+    }
+}
+console.log(meta.value);
+
+// function // arrow function
+const criarMeta = () => {}
+
+// named function
+function criarMetas() {}
